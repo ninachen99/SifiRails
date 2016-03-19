da2888303f20131af7a673346fb90b2d7f6a19a7
Nina Chen <ninacode99@gmail.com>
2016-03-19 10:36:24 -0500

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,12 @@
 t1 = Theater.create(name: "Red", seats: 100)
 t2 = Theater.create(name: "Yellow", seats: 120)
 t3 = Theater.create(name: "Blue", seats: 98)
-m1 = Movie.create(title: "Sherlock", image: "http://s3.amazonaws.com/codecademy-content/courses/learn-rails/img/sherlock-holmes.jpg", release_year: "2009", plot: "When a string of brutal murders terrorizes London, it doesn't take long for legendary detective Sherlock Holmes (Robert Downey Jr.) and ...", theater_id: t1.id)
 
-m2 = Movie.create(title: "Closer", image: "http://s3.amazonaws.com/codecademy-content/courses/learn-rails/img/closer.jpg", release_year: "2004", plot: "Alice (Natalie Portman), who has moved to London, meets Dan (Jude Law) on the street. While looking at him, a taxi hits her. After taking ...", theater_id: t2.id)
+m1 = Movie.create(title: "Sherlock", image: "http://s3.amazonaws.com/codecademy-content/courses/learn-rails/img/sherlock-holmes.jpg", release_year: "2016", plot: "When a string of brutal murders terrorizes London, it doesn't take long for legendary detective Sherlock Holmes (Robert Downey Jr.) and ...", theater_id: t1.id)
 
-m3 = Movie.create(title: "Interhill", image: "http://s3.amazonaws.com/codecademy-content/courses/learn-rails/img/interstellar.jpg", release_year: "2014", plot: "In Earth's future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand Michael Cai...", theater_id: t3.id)
+m2 = Movie.create(title: "Closer", image: "http://s3.amazonaws.com/codecademy-content/courses/learn-rails/img/closer.jpg", release_year: "2016", plot: "Alice (Natalie Portman), who has moved to London, meets Dan (Jude Law) on the street. While looking at him, a taxi hits her. After taking ...", theater_id: t2.id)
+
+m3 = Movie.create(title: "Interhill", image: "http://s3.amazonaws.com/codecademy-content/courses/learn-rails/img/interstellar.jpg", release_year: "2016", plot: "In Earth's future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand Michael Cai...", theater_id: t3.id)
 # seeds showtimes
 s1 = Showtime.create(month: "12", date: "12", hour: "14:30", year: "2016", movie_id:m1.id, theater_id: t1.id)
 
@@ -32,7 +33,7 @@ s3 = Showtime.create(month: "08", date: "15", hour: "15:40", year: "2016", movie
 
 # movie has many showtimes
 
-m1.showtimes << [s1, s2]
+m1.showtimes << [s1]
 m2.showtimes << [s2, s3]
 m3.showtimes << [s1, s3]
 
