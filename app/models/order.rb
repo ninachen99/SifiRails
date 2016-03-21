2b435d6ed7404940489f089d12df208945e25fd4
Nina Chen <ninacode99@gmail.com>
2016-03-21 11:38:41 -0500

    Update views models

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ActiveRecord::Base
 	has_one :movie, through: :showtime
 
 	validates :first_name, :last_name, :age, :email, :credit_card, :expiration, :showtime_id, :movie_id, :order_quantity, presence: true
-	validate :order_quantity, if: :orders_sold_out?
+	#validate :order_quantity, if: :orders_sold_out?
 	#validate :age, if: :age_restriction?
     # find order sum:  Showtime.find(params[:id]).orders.sum("order_quantity") 
     # find the seats:
@@ -11,16 +11,20 @@ class Order < ActiveRecord::Base
     # Theater.find(theaterId).seats
 
     #order_total = Order.where("showtime_id = ?", params[:orders]).count
-    
+=begin  
     def order_total
-       puts Order.where("showtime_id", self.showtime_id).sum("order_quantity")
-       return Order.where("showtime_id", self.showtime_id).sum("order_quantity")
-       
-    end 
+       puts "9" * 50
+       total = Showtime.find(params[:id]).orders.sum("order_quantity")
+       return total
+       #puts Order.where("showtime_id", self.showtime_id).sum("order_quantity")
+       #return Order.where("showtime_id", self.showtime_id).sum("order_quantity")
+    end
 
     def order_limit
-        newId = Showtime.find_by("id", self.id).theater_id
-    	available_seats = Theater.find_by("id", newId).seats
+        theaterId = Showtime.find(params[:id]).theater_id
+        available_seats = Theater.find(theaterId).seats
+        #newId = Showtime.find_by("id", self.id).theater_id
+    	#available_seats = Theater.find_by("id", newId).seats
         puts available_seats
         return available_seats
     end 
@@ -31,14 +35,12 @@ class Order < ActiveRecord::Base
     		errors.add(:order_quantity, "All seats are sold out for this movie.")
     	end 
     end 
-
+=end 
     def age_restriction?
     	order = Order.new
     	user_age = order.age.to_i
     	if user_age < 18
     		errors.add(:age, "You must be 18 or older to make a purchase.")
     	end 
-
     end 
-
 end
