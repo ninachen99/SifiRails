0709f062082f203d5b53c201e1573e2f41f120df
Nina Chen <ninacode99@gmail.com>
2016-03-24 13:52:43 -0500

    Update order model

---
+++ app/helpers/theaters_helper.rb
@@ -0,0 +1,4 @@
+module TheatersHelper
+
+	
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,59 +1,44 @@
 class Order < ActiveRecord::Base
     #attr_accessible :movie_id, :showtime_id
-    #before_save :override_field
 
 	belongs_to :showtime
 	has_one :movie, through: :showtime
 
 	validates :first_name, :last_name, :age, :email, :credit_card, :expiration, :showtime_id, :movie_id, :order_quantity, presence: true
-	#validate :order_quantity, if: :orders_sold_out?
-	#validate :age, if: :age_restriction?
-    # find order sum:  Showtime.find(params[:id]).orders.sum("order_quantity") 
-    # find the seats:
-    # theaterId = Showtime.find(params[:id]).theater_id
-    # Theater.find(theaterId).seats
-
-    #order_total = Order.where("showtime_id = ?", params[:orders]).count
- 
+	validate :order_quantity, if: :orders_sold_out?
+	validate :age, if: :age_restriction?
+    
     def order_total
        puts "9" * 50
-       total = Showtime.find(params[:id]).orders.sum("order_quantity")
-       return total
-       #puts Order.where("showtime_id", self.showtime_id).sum("order_quantity")
-       #return Order.where("showtime_id", self.showtime_id).sum("order_quantity")
+       puts Order.where("showtime_id", self.showtime_id).sum("order_quantity")
+       return Order.where("showtime_id", self.showtime_id).sum("order_quantity")
     end
 
+    # find the theater seats capacity:
     def order_limit
-        puts "9" * 50
-        theaterId = Showtime.find(params[:id]).theater_id
-        available_seats = Theater.find(theaterId).seats
-        #newId = Showtime.find_by("id", self.id).theater_id
-    	#available_seats = Theater.find_by("id", newId).seats
-        puts available_seats
+        puts "&" * 50
+        showing = self.showtime_id.to_i
+        puts showing
+        show_room = Showtime.find(showing).theater_id.to_i
+        puts show_room
+        available_seats = Theater.find(show_room).seats
+        puts available_seats 
         return available_seats
     end 
 
     def orders_sold_out?
     	if order_total >= order_limit
+            puts "$total" * 100
             puts order_total
     		errors.add(:order_quantity, "All seats are sold out for this movie.")
     	end 
     end 
 
     def age_restriction?
-    	order = Order.new
-    	user_age = order.age.to_i
-    	if user_age < 18
-    		errors.add(:age, "You must be 18 or older to make a purchase.")
-    	end 
-    end 
-=begin
-    private
-
-    def override_field
-        if self.movie_id.nil?
-            self.movie_id = showtime.movie_id
+    	user_age = self.age 
+        if user_age <= 18
+    	  errors.add(:age, "You must be 18 or older to make a purchase.")
         end 
     end 
-=end
+
 end
